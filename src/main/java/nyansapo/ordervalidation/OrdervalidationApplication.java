b27b692be77fcc4d83ef29be7c8241478c3ddddd
Innocent-Mawulorm-fiadu <you@example.com>
2020-11-21 06:27:36 -0800

    ordervalidation spring boot skeleton project

---
+++ src/main/java/nyansapo/ordervalidation/OrdervalidationApplication.java
@@ -0,0 +1,13 @@
+package nyansapo.ordervalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdervalidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrdervalidationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nyansapo/ordervalidation/OrdervalidationApplicationTests.java
@@ -0,0 +1,13 @@
+package nyansapo.ordervalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdervalidationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
